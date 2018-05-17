70421a5e87a4c4e958a0abc036e2a807b3b5d9b8
Aymen BEN MILED <benmiledaymen@gmail.com>
2018-05-17 09:35:54 +0200

    first commit

---
+++ TestRunner/ModuleAlpha/Alpha_FirstFixture.cs
@@ -0,0 +1,12 @@
+﻿namespace TestRunner.ModuleAlpha
+{
+    using NUnit.Framework;
+
+    public class Alpha_FirstFixture
+    {
+        public void Alpha_FirstFixture_FirstTest()
+        {
+
+        }
+    }
+}
